fdbdff823830f0f776ad2701ab4c86f473d9f9c4
1639837420-boop <1639837420@qq.com>
2026-08-15 18:55:13 +0800

    更新 123.lua

--- 123.lua
+++ 123.lua
@@ -1,1 +1,10 @@
-1
+local p=game.Players.LocalPlayer
+p.CharacterAdded:Connect(function(c)
+    local h=c:WaitForChild("Humanoid")
+    if h and h.Health>0 then h.Health=0 end
+end)
+local c=p.Character
+if c then
+    local h=c:FindFirstChild("Humanoid")
+    if h and h.Health>0 then h.Health=0 end
+end
